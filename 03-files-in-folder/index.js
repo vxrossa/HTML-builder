9e9c1692708259cd883bdf50e94685fa23bb5b10
vxrossa <ibasalaev@icloud.com>
2021-10-30 22:40:15 +0300

    feat: solve third task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs').promises;
+const files = require('fs');
 const process = require('process');
 const path = require('path');
 
@@ -7,15 +8,20 @@ async function readDirectory(dir){
   const file = await fs.readdir(dir, {
     withFileTypes: true,
   });
-  console.log(file);
   for (let i = 0; i < file.length; i++){
-    const name = file[i].name.substring(0,file[i].name.indexOf('.'))
-    const ext = path.extname(file[i].name).substring(1);
-    if(ext.length > 0 && file[i].isFile()){
-      console.log(name + ' ' + ext);
-    } else {
-      console.log('FOLDER');
-    }
+    await files.stat(dir + '/' + file[i].name, (err,stats ) => {
+      const size = Math.ceil(stats.size / 1024); // in kb
+      const name = file[i].name.substring(0,file[i].name.indexOf('.'));
+      const ext = path.extname(file[i].name).substring(1);
+      if(ext.length > 0 && file[i].isFile()){
+        console.log(`${name} - ${ext} - ${size}kb`);
+      } else {
+        console.log('FOLDER');
+      }
+    })
+
+
+
 
     /*const name = elem.substring(0,elem.indexOf('.'));
     const ext = path.extname(elem).substring(1);*/
@@ -24,4 +30,11 @@ async function readDirectory(dir){
   }
 }
 
-readDirectory(__dirname + '\\secret-folder');
+async function getSize(dir){
+  await file.stat(dir, (err, stats) => {
+    console.log(stats);
+  })
+}
+
+readDirectory(__dirname + '/secret-folder');
+
