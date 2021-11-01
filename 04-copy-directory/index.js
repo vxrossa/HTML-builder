4204626deb5ef97e9f0caf6aa8740f5e8b3c95a6
vxrossa <ibasalaev@icloud.com>
2021-11-01 15:49:02 +0300

    feat: solve 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs').promises;
 
+async function copyDir(directory){
+  try{
+    await fs.mkdir(__dirname + directory);
+    console.log('Directory successfully created.');
+  }
+  catch{
+    console.log('The directory already exists.');
+  }
+  try{
+    const files = await fs.readdir(__dirname + '/files');
+    if(files.length == 0){
+      console.log('No files inside. Only created the folder.');
+    }
+    for(let i = 0; i < files.length; i++){
+      await fs.copyFile(__dirname + `/files/${files[i]}`,__dirname + `/files-copy/${files[i]}`);
+    }
+    console.log('Files successfully copied.');
+  }
+  catch{
+    console.log('Error!');
+  }
+}
+
+copyDir('/files-copy');
