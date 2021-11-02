77a3dad61b2cabca1a09e0cbd35053364228eb86
vxrossa <ibasalaev@icloud.com>
2021-11-02 12:30:34 +0300

    refactor: remove comments

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,24 +2,6 @@ const fsp = require('fs').promises;
 const fs = require('fs');
 const path = require('path');
 
-// async function buildStyles(dir){
-//   let copyText = '';
-//   const regExp = /.css$/i;
-//   let files = await (await fsp.readdir(dir)).filter(style => regExp.test(style));
-//   console.log(files);
-
-//   files.forEach(file => {
-//     const readStream = fs.createReadStream(__dirname + `/styles/${file}`);
-//     readStream.on('data', part => {
-//       copyText += part;
-//       const writeStream = fs.createWriteStream(__dirname + '/project-dist/bundle.css',{
-//         flags: 'a',
-//       });
-//       writeStream.write(copyText);
-//     });
-//   });
-// }
-
 async function buildStyles(dir){
   let copyText = '';
   const files = await fsp.readdir(dir,{
