11668f555ae1f1ad93c31ac8d365c36fcc0cd312
vxrossa <ibasalaev@icloud.com>
2021-11-03 00:06:35 +0300

    feat: add html/css/asset bundling to 6th task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -26,16 +26,21 @@ async function buildPage(dir){
       console.log('ERROR MOVING ASSETS');
     }
   }
-  // try{
-  //   await fsp.mkdir(__dirname + dir);
-  // }
-  // catch{
-  //   console.log('Error creating dist folder.')
-  // }
 }
 
 async function buildHTML(dir){
-
+  const components = await fsp.readdir(__dirname + '/components', { withFileTypes: true });
+  let template = await fsp.readFile(__dirname + '/template.html', 'utf-8');
+  for(let i = 0; i < components.length; i++){
+    const readStream = fs.createReadStream(__dirname + `/components/${components[i].name}`);
+    readStream.on('data', part => {
+      template = template.replace(`{{${path.basename(components[i].name, '.html')}}}`,part.toString());
+      if(i == components.length - 1){
+        const writeStream = fs.createWriteStream(__dirname + `/project-dist/index.html`,{ flags: 'w'});
+        writeStream.write(template);
+      }
+    });
+  }
 }
 
 async function buildCSS(dir){
@@ -46,7 +51,7 @@ async function buildCSS(dir){
       const readStream = fs.createReadStream(__dirname + `/styles/${file.name}`);
       readStream.on('data', part => {
         cssText += part;
-        const writeStream = fs.createWriteStream(__dirname + `/project-dist/style.css`,{ flags: 'a' });
+        const writeStream = fs.createWriteStream(__dirname + `/project-dist/style.css`,{ flags: 'w' });
         writeStream.write(cssText);
       })
     }
@@ -61,21 +66,21 @@ async function getAssets(dir){
     for (let elem of files){
       if(elem.isDirectory()){
         try{
-          await fsp.stat(__dirname + `/project-dist/${elem.name}`);
+          await fsp.access(__dirname + `/project-dist/assets`);
         }
-        catch{
-          await fsp.mkdir(__dirname + `/project-dist/${elem.name}`);
+        catch(err){
+          await fsp.mkdir(__dirname + '/project-dist/assets');
         }
-        // try{
-        //   fsp.mkdir(__dirname + `/project-dist/${elem.name}`);
-        // }
-        // catch{
-        //   console.log('Error copying files');
-        // }
+        try{
+          await fsp.access(__dirname + `/project-dist${dir}/${elem.name}`);
+        }
+        catch(err){
+          await fsp.mkdir(__dirname + `/project-dist${dir}/${elem.name}`);
+        }
         try{
           const filesinside = await fsp.readdir(__dirname + dir + `/${elem.name}`);
           filesinside.forEach(fileInside => {
-            fsp.copyFile(__dirname + dir + `/${elem.name}/${fileInside}`,__dirname + `/project-dist/${elem.name}/${fileInside}`);
+            fsp.copyFile(__dirname + dir + `/${elem.name}/${fileInside}`,__dirname + `/project-dist/assets/${elem.name}/${fileInside}`);
           });
         }
         catch{
@@ -95,10 +100,15 @@ async function getAssets(dir){
     console.log('Styles bundled');
   }
   catch{
-    console.log('Error bundling styles');
+    console.log('Error bundling styles.');
+  }
+  try{
+    await buildHTML('/components');
+    console.log('HTML components bundled.')
+  }
+  catch{
+    console.log('Error bundling components.')
   }
 }
+
 buildPage('/project-dist');
-// buildHTML('/components');
-// buildCSS('/styles');
-// getAssets('/assets');
