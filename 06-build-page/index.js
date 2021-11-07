6bb4bd85fca758844cc169109bba20593aa21d4e
vxrossa <ibasalaev@icloud.com>
2021-11-07 22:02:42 +0300

    feat: migrate new code to index.js file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,108 +1,105 @@
-const fsp = require('fs').promises;
-const fs = require('fs');
+const fs = require('fs/promises');
+const fss = require('fs');
 const path = require('path');
 
-const outputDir = __dirname + '/project-dist';
-console.log(outputDir);
-
-async function buildPage(dir){
-  try{
-    await fsp.stat(__dirname + dir);
-    console.log('Dist folder found. Updating files.');
-    try{
-      await getAssets('/assets');
-    }
-    catch{
-      console.log('ERROR MOVING ASSETS');
-    }
-  }
-  catch(err){
-    console.log('Dist folder initialized. Copying files.');
-    await fsp.mkdir(__dirname + '/project-dist');
-    try{
-      await getAssets('/assets');
-    }
-    catch{
-      console.log('ERROR MOVING ASSETS');
-    }
-  }
-}
-
-async function buildHTML(){
-  const components = await fsp.readdir(__dirname + '/components', { withFileTypes: true });
-  let template = await fsp.readFile(__dirname + '/template.html', 'utf-8');
-  for(let i = 0; i < components.length; i++){
-    const readStream = fs.createReadStream(__dirname + `/components/${components[i].name}`);
-    readStream.on('data', part => {
-      template = template.replace(`{{${path.basename(components[i].name, '.html')}}}`,part.toString());
-      if(i == components.length - 1){
-        const writeStream = fs.createWriteStream(__dirname + '/project-dist/index.html',{ flags: 'w'});
-        writeStream.write(template);
-      }
-    });
-  }
-}
-
-async function buildCSS(dir){
-  let cssText = '';
-  const cssFiles = await fsp.readdir(__dirname + dir, { withFileTypes: true });
-  cssFiles.forEach(file => {
-    if(file.isFile() && path.extname(file.name) == '.css'){
-      const readStream = fs.createReadStream(__dirname + `/styles/${file.name}`);
-      readStream.on('data', part => {
-        cssText += part;
-        const writeStream = fs.createWriteStream(__dirname + '/project-dist/style.css',{ flags: 'w' });
-        writeStream.write(cssText);
-      });
-    }
-  });
-}
+let filesList = [];
+let dirList = [];
 
 async function getAssets(dir){
   try{
-    const files = await fsp.readdir(__dirname + dir, {
-      withFileTypes: true,
-    });
-    for (let elem of files){
-      if(elem.isDirectory()){
-        try{
-          await fsp.access(__dirname + `/project-dist${dir}/${elem.name}`);
-        }
-        catch(err){
-          await fsp.mkdir(__dirname + `/project-dist${dir}/${elem.name}`, {recursive: true});
-        }
-        try{
-          const filesinside = await fsp.readdir(__dirname + dir + `/${elem.name}`);
-          filesinside.forEach(fileInside => {
-            fsp.copyFile(__dirname + dir + `/${elem.name}/${fileInside}`,__dirname + `/project-dist/assets/${elem.name}/${fileInside}`);
-          });
-        }
-        catch{
-          console.log('error');
-        }
+    const files = await fs.readdir(path.join(__dirname,dir), { withFileTypes: true});
+    for (let i = 0; i < files.length; i++) {
+      if(files[i].isDirectory()){
+        await getAssets(path.join(dir,files[i].name));
+        dirList.push(`${dir}/${files[i].name}`);
       }
-      else{
-        console.log('file!');
+      else if(files[i].isFile()){
+        filesList.push(`${dir}/${files[i].name}`);
       }
     }
   }
   catch{
-    console.log('Error reading directory.');
-  }
-  try{
-    await buildCSS('/styles');
-    console.log('Styles bundled');
-  }
-  catch{
-    console.log('Error bundling styles.');
-  }
-  try{
-    await buildHTML('/components');
-    console.log('HTML components bundled.');
-  }
-  catch{
-    console.log('Error bundling components.');
+    console.log('Error reading assets');
   }
 }
 
-buildPage('/project-dist');
+async function copyAssets(dir){
+  try{
+    await getAssets('assets');
+    console.log('Assets folder successfully copied.');
+  }
+  catch{
+    console.log('Error reading assets folder.');
+  }
+  for(let i = 0; i < dirList.length; i++){
+    try{
+      await fs.mkdir(path.join(__dirname,dir,dirList[i]),{ recursive: true });
+    }
+    catch{
+      console.log('Error creating folders.');
+    }
+  }
+  for(let i = 0; i < filesList.length; i++){
+    try{
+      await fs.copyFile(
+        path.join(__dirname,filesList[i]),
+        path.join(__dirname,dir,filesList[i])
+      );
+    }
+    catch{
+      console.log('Error copying files to folders.');
+    }
+  }
+}
+
+async function buildHTML(dir,src){
+  try{
+    await fs.mkdir(path.join(__dirname,'project-dist'));
+    console.log('Dist folder created');
+  }
+  catch{
+    console.log('Dist folder already created.');
+  }
+  const components = await fs.readdir(path.join(__dirname,dir), { withFileTypes: true });
+
+  let template = await fs.readFile(path.join(__dirname,src),'utf-8');
+  try{
+    for(let i = 0; i < components.length; i++){
+      if(components[i].isFile() && path.extname(components[i].name) == '.html'){
+        const readStream = fss.createReadStream(path.join(__dirname,dir,components[i].name));
+        readStream.on('data',part => {
+          template = template.replace(`{{${path.basename(components[i].name,'.html')}}}`,part);
+            const writeStream = fss.createWriteStream(path.join(
+              __dirname,'project-dist','index.html'
+            ), {flags: 'w'});
+            writeStream.write(template);
+        });
+      }
+    }
+  console.log('HTML components bundled');
+  }
+  catch{
+    console.log('Error bundling HTML.')
+  }
+}
+
+async function buildCSS(dir,output){
+  let cssText = '';
+  const cssFiles = await fs.readdir(path.join(__dirname,dir), {withFileTypes: true});
+  cssFiles.forEach(file => {
+    if(file.isFile() && path.extname(file.name) == '.css'){
+      const readStream = fss.createReadStream(path.join(__dirname,dir,file.name));
+      readStream.on('data', part => {
+        cssText += part;
+        const writeStream = fss.createWriteStream(path.join(__dirname,'project-dist',output), {flags: 'w'});
+        writeStream.write(cssText);
+      });
+    }
+  })
+  console.log('CSS styles bundled.');
+}
+
+
+copyAssets('project-dist');
+buildHTML('components','template.html');
+buildCSS('styles','style.css');
